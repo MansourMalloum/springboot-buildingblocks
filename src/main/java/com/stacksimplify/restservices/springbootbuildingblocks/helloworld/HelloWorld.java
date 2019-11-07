9b264ef3dc80655dd4a5eab89fe82fdd97867274
mansour.malloum <mansourmalloum@gmail.com>
2019-11-07 16:15:38 +0100

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices.springbootbuildingblocks.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	//@RequestMapping(method = RequestMethod.GET, path = "/hello")
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello from me 1:";
+	}
+	
+	
+	//userDetails
+	@GetMapping("/hello-bean")
+	public UserDetails userDetails() {
+		return new UserDetails("Mansour", "Malloum", "YDE 2");
+	}
+}
